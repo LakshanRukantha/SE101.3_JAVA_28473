c1aca36517330bf91389ec347a193fd0f1db0c83
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-07-31 22:21:48 +0530

    Lab Sheet 07

---
+++ Lab-07-2023.06.15/Q3/Student.java
@@ -0,0 +1,10 @@
+package com.mycompany.lab7.q3;
+
+
+final class Student {
+    final int marks=100;
+    final void display(){
+        System.out.println(marks);
+    }
+    
+}
---
+++ Lab-07-2023.06.15/Q4/Lab7Q4.java
@@ -0,0 +1,8 @@
+package com.mycompany.lab7.q4;
+
+public class Lab7Q4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
